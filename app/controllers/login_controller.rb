b05485d92c1d19c9ac123fad75edbed9c52dab8e
trivektor <trivektor@gmail.com>
2013-08-07 00:38:03 -0700

    complete rendering of login form

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -15,16 +15,24 @@ class LoginController < UIViewController
 
   def performHouseKeepingTasks
     createCustomCells
-    @loginTable = UITableView.alloc.initWithFrame(self.view.bounds, style: UITableViewStyleGrouped)
+
+    containerView = UIView.alloc.initWithFrame(self.view.bounds)
+    containerView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
+    self.view.addSubview containerView
+
+    @loginTable = UITableView.alloc.initWithFrame(containerView.bounds, style: UITableViewStyleGrouped)
     @loginTable.delegate = self
     @loginTable.dataSource = self
     @loginTable.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
-    self.view.addSubview @loginTable
+    @loginTable.backgroundView = nil
+    containerView.addSubview @loginTable
 
-    submitButton = UIButton.alloc.initWithFrame(CGRectMake(44, 148, 936, 44), style: UIButtonTypeRoundedRect)
-    submitButton.titleLabel.text = 'Sign in'
-    self.view.insertSubview(submitButton, aboveSubview:@loginTable)
-    self.view.bringSubviewToFront(submitButton)
+    submitButton = UIButton.alloc.initWithFrame(CGRectMake(44, 143, 936, 44), style: UIButtonTypeRoundedRect)
+    submitButton.setTitle('Submit', forState: UIControlStateNormal)
+    submitButton.titleLabel.textColor = UIColor.whiteColor
+    submitButton.backgroundColor = UIColor.colorWithRed(72/255.0, green: 201/255.0, blue: 176/255.0, alpha: 1.0)
+    submitButton.layer.cornerRadius = 6.0
+    containerView.addSubview(submitButton, aboveSubview: @loginTable)
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
@@ -45,15 +53,17 @@ class LoginController < UIViewController
     # TODO: refactor
     @usernameCell = UITableViewCell.alloc.initWithFrame(CGRectMake(0, 0, 1024, 44))
     @usernameCell.selectionStyle = UITableViewCellSelectionStyleNone
-    usernameField = UITextField.alloc.initWithFrame(CGRectMake(8, 7, 1009, 30))
+    usernameField = UITextField.alloc.initWithFrame(CGRectMake(8, 7, 916, 30))
     usernameField.attributedPlaceholder = NSAttributedString.alloc.initWithString('Username or email')
+    @usernameCell.backgroundColor = UIColor.whiteColor
     @usernameCell.contentView.addSubview(usernameField)
 
     @passwordCell = UITableViewCell.alloc.initWithFrame(CGRectMake(0, 0, 1024, 44))
     @passwordCell.selectionStyle = UITableViewCellSelectionStyleNone
-    passwordField = UITextField.alloc.initWithFrame(CGRectMake(8, 7, 1009, 30))
+    passwordField = UITextField.alloc.initWithFrame(CGRectMake(8, 7, 916, 30))
     passwordField.attributedPlaceholder = NSAttributedString.alloc.initWithString('Password')
     passwordField.secureTextEntry = true
+    @passwordCell.backgroundColor = UIColor.whiteColor
     @passwordCell.contentView.addSubview(passwordField)
   end
 
