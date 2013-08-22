d803ffd1a82b4fc2bd06f67439dcd4c52b399b1f
trivektor <trivektor@gmail.com>
2013-08-21 20:08:32 -0700

    add app constants

--- app/config/constants.rb
+++ app/config/constants.rb
@@ -1,6 +1,8 @@
+APP_NAME = 'Gitpad Motion'
 GITHUB_API_HOST = 'https://api.github.com'
 OAUTH_PARAMS = {
   scopes: ['user', 'user:follow', 'public_repo', 'repo', 'repo:status', 'delete_repo', 'notifications', 'gist'],
   client_id: ENV['GITPAD_MOTION_ID'],
   client_secret: ENV['GITPAD_MOTION_SECRET']
 }
+APP_KEYCHAIN_IDENTIFIER = 'GitpadMotionKeychain1'
