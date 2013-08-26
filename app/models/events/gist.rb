79cff59529404a894b587e6acc01551cc092e9c5
trivektor <trivektor@gmail.com>
2013-08-25 18:33:42 -0700

    add GistEvent

--- app/models/events/gist.rb
+++ app/models/events/gist.rb
@@ -1,2 +1,14 @@
 class GistEvent < TimelineEvent
+
+  def toString
+    actor = self.actor.login
+    gist = self.gist.id
+    action = self.payload[:action]
+    "#{actor} #{action} gist:#{gist}"
+  end
+
+  def gist
+    Gist.new(self.payload[:gist])
+  end
+
 end
---
+++ app/models/gist.rb
@@ -0,0 +1,45 @@
+class Gist
+
+  attr_accessor :data
+
+  def initialize(data={})
+    @data = data
+  end
+
+  def id
+    @data[:id]
+  end
+
+  def description
+    @data[:description]
+  end
+
+  def created_at
+    @data[:created_at]
+  end
+
+  def url
+    @data[:url]
+  end
+
+  def htmlUrl
+    @data[:html_url]
+  end
+
+  def commentsUrl
+    @data[:comments_url]
+  end
+
+  def numFiles
+    @data[:files].to_i
+  end
+
+  def numForks
+    @data[:forks].to_i
+  end
+
+  def numComments
+    @data[:comments].to_i
+  end
+
+end
