2177c17b66b43aae4961794ea6efaf65c76f966d
trivektor <trivektor@gmail.com>
2013-08-25 20:07:12 -0700

    add 'toString' methods for Push and PullRequestReview events

--- app/models/events/pull_request_review.rb
+++ app/models/events/pull_request_review.rb
@@ -1,2 +1,8 @@
 class PullRequestReviewEvent < TimelineEvent
+
+  def toString
+    payload = self.payload
+    "#{actor.login} commented on pull request #{repo.name}/#{payload[:number]}"
+  end
+
 end
--- app/models/events/push.rb
+++ app/models/events/push.rb
@@ -1,2 +1,9 @@
 class PushEvent < TimelineEvent
+
+  def toString
+    payload = self.payload
+    ref = payload[:ref].to_s.split('/')
+    "#{actor.login} pushed to #{repo.name} at #{ref.last}"
+  end
+
 end
