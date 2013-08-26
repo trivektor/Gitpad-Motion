e66893d90b5d510b4b83c48ffad7794963d0b4e4
trivektor <trivektor@gmail.com>
2013-08-25 19:48:11 -0700

    add 'toString' method for IssuesEvent and MemberEvent

--- app/models/events/issues.rb
+++ app/models/events/issues.rb
@@ -1,2 +1,12 @@
 class IssuesEvent < TimelineEvent
+
+  def toString
+    action = payload[:action]
+    "#{actor.login} #{action} issue #{repo.name}##{issue.number}"
+  end
+
+  def issue
+    Issue.new(payload[:issue])
+  end
+
 end
--- app/models/events/member.rb
+++ app/models/events/member.rb
@@ -1,2 +1,11 @@
 class MemberEvent < TimelineEvent
+
+  def toString
+    "#{actor.login} added #{member.login} to #{repo.name}"
+  end
+
+  def member
+    User.new(payload[:member])
+  end
+
 end
