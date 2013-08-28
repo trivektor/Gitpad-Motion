c724837276e22bade02bda4ba5a9edefda127d14
trivektor <trivektor@gmail.com>
2013-08-27 23:43:41 -0700

    prep work to load local html

--- app/controllers/newsfeed_details_controller.rb
+++ app/controllers/newsfeed_details_controller.rb
@@ -11,13 +11,34 @@ class NewsfeedDetailsController < UIViewController
     super
     self.navigationItem.title = 'Details'
     performHousekeepingTasks
+    loadNewsfeed
   end
 
   def performHousekeepingTasks
     createBackButton
     @wview = UIWebView.alloc.initWithFrame(self.view.bounds)
     @wview.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
+    @wview.delegate = self
     self.view.addSubview(@wview)
+  end
+
+  def loadNewsfeed
+    mainBundle = NSBundle.mainBundle
+
+    html = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/newsfeed', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    contentHtml = html.stringByReplacingOccurrencesOfString('%@', withString: 'abc')
+
+    data = contentHtml.dataUsingEncoding(NSUTF8StringEncoding)
+    @wview.loadData(data, MIMEType: 'text/html', textEncodingName: 'utf-8', baseURL: NSURL.fileURLWithPath(mainBundle.bundlePath))
+    # @wview.loadHTMLString(contentHtml, baseUrl: )
+  end
+
+  def webView(webView, shouldStartLoadWithRequest: request, navigationType: navigationType)
+    true
   end
 
 end
