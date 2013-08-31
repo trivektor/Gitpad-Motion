0f1301fb72c1a546a393d43bda4bda4f9e6d2d5b
trivektor <trivektor@gmail.com>
2013-08-30 23:07:01 -0700

    add toHtmlString for Follow and Gist events

--- app/models/events/follow.rb
+++ app/models/events/follow.rb
@@ -6,4 +6,54 @@ class FollowEvent < TimelineEvent
     "#{actor} started following #{target}"
   end
 
+  def toHtmlString
+    actor = self.actor
+    targetActor = self.targetActor
+
+    mainBundle = NSBundle.mainBundle
+
+    actorHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_actor', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    targetActorHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_actor', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    actionHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_action', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    actorHtml = actorHtml.stringByReplacingOccurrencesOfString('{{actor}}', withString: 'actor:')
+             .stringByReplacingOccurrencesOfString('{{avatar}}', withString: actor.avatarUrl)
+             .stringByReplacingOccurrencesOfString('{{login}}', withString: actor.login)
+
+    targetActorHtml = targetActorHtml.stringByReplacingOccurrencesOfString('{{actor}}', withString: 'actor:')
+                   .stringByReplacingOccurrencesOfString('{{avatar}}', withString: targetActor.avatarUrl)
+                   .stringByReplacingOccurrencesOfString('{{login}}', withString: targetActor.login)
+
+    actionHtml = actionHtml.stringByReplacingOccurrencesOfString('{{action}}', withString: 'started following')
+
+    array = NSArray.alloc.initWithArray([actorHtml, actionHtml, targetActorHtml])
+    array.componentsJoinedByString('')
+  end
+
+  private
+
+  def toHTMLStringForObject1WithName(name1, AndAvatar1: avatar1, Object2: name2, AndAvatar2: avatar2, AndAction: actionName)
+    actorHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_actor', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    actionHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_action', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+  end
+
 end
--- app/models/events/gist.rb
+++ app/models/events/gist.rb
@@ -7,6 +7,42 @@ class GistEvent < TimelineEvent
     "#{actor} #{action} gist:#{gist}"
   end
 
+  def toHtmlString
+    payload = self.payload
+    actor = self.actor
+    gist = self.gist
+
+    mainBundle = NSBundle.mainBundle
+
+    actorHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_actor', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    gistHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_actor', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    actionHtml = NSString.stringWithContentsOfFile(
+      mainBundle.pathForResource('html/event_action', ofType: 'html'),
+      encoding: NSASCIIStringEncoding,
+      error: nil)
+
+    actorHtml = actorHtml.stringByReplacingOccurrencesOfString('{{actor}}', withString: 'actor:')
+                         .stringByReplacingOccurrencesOfString('{{avatar}}', withString: actor.avatarUrl)
+                         .stringByReplacingOccurrencesOfString('{{login}}', withString: actor.login)
+
+    gistHtml = gistHtml.stringByReplacingOccurrencesOfString('{{actor}}', withString: 'gist:')
+                       .stringByReplacingOccurrencesOfString('{{avatar}}', withString: GITHUB_OCTOCAT)
+                       .stringByReplacingOccurrencesOfString('{{login}}', withString: "gist:#{gist.id}")
+
+    actionHtml = actionHtml.stringByReplacingOccurrencesOfString('{{action}}', withString: payload[:action])
+
+    array = NSArray.alloc.initWithArray([actorHtml, actionHtml, gistHtml])
+    array.componentsJoinedByString('')
+  end
+
   def gist
     Gist.new(self.payload[:gist])
   end
