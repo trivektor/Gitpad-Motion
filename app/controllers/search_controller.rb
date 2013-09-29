4bc6ee1f9558104df715b36330e128b0793269da
trivektor <trivektor@gmail.com>
2013-09-29 02:47:41 -0700

    add search bar and results table to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,39 @@
 class SearchController < UIViewController
 
+  attr_accessor :searchBar, :searchResults
+
   def viewDidLoad
     super
+    @searchResults = []
     performHousekeepingTasks
   end
 
   def performHousekeepingTasks
     self.navigationItem.title = 'Search'
+    buildSearchBar
+    @table = createTable(frame: [[0, 44], [1024, 1024]])
+    self.view.addSubview(@table)
+  end
 
-    view = UIView.alloc.initWithFrame(self.view.bounds)
-    view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
-    view.setBackgroundColor(UIColor.whiteColor)
+  def buildSearchBar
+    @searchBar = UISearchBar.alloc.initWithFrame([[0, 0], [1024, 44]])
+    self.view.addSubview(@searchBar)
+  end
 
-    self.view.addSubview(view)
+  def numberOfSectionsInTableView(tableView)
+    1
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @searchResults.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    cell = @table.dequeueReusableCellWithIdentifier('Cell') || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: 'Cell')
+    end
+
+    cell
   end
 
 end
