42f200a3dbfc827620ee50aadc1b064697673b64
trivektor <trivektor@gmail.com>
2013-10-05 11:06:17 -0700

    fix bug which broke authentication

--- app/afnetworking.rb
+++ app/afnetworking.rb
@@ -4,7 +4,7 @@ module AFNetWorking
     @httpClient ||= AFMotion::Client.build_shared(options[:base_url] || GITHUB_API_HOST) do
       header "Accept", "application/json"
       authorization(options[:authorization]) if options[:authorization]
-      parameter_encoding :json unless !options[:parameter_encoding]
+      parameter_encoding :json unless options[:parameter_encoding] === false
       operation :json
     end
   end
